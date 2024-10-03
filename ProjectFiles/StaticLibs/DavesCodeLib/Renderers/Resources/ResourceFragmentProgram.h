0521b6d58dceaa33f590ceba29b698cdcbd05a8b
David Cradock <djpcradock@gmail.com>
2024-10-03 14:10:48 +0100

    Added beginnings of vertex and fragment resources

--- ProjectFiles/StaticLibs/DavesCodeLib/Renderers/Resources/ResourceFragmentProgram.h
+++ ProjectFiles/StaticLibs/DavesCodeLib/Renderers/Resources/ResourceFragmentProgram.h
@@ -1,9 +1,9 @@
-#ifndef RESOURCE_VERTEXBUFFER_H
-#define RESOURCE_VERTEXBUFFER_H
+#ifndef RESOURCE_FRAGMENTPROGRAM_H
+#define RESOURCE_FRAGMENTPROGRAM_H
 
 namespace DCL
 {
-	/// \brief Base class for resource of type vertex buffer
+	/// \brief Base class for resource of type fragment program
 	///
 	/// We derive resource classes from this base class for each renderer type such as OpenGL and Vulkan.
 	/// These classes are to be declared within this header file to reduce the number of files.
@@ -12,45 +12,36 @@ namespace DCL
 	/// I know it means that the various renderers for the different graphics APIs are including stuff which isn't related to them, but that's a small price to pay for the reduced number of files we'd otherwise have to create.
 	/// Within DCL, all code uses only a pointer to the base class and the implementation for each of the methods are implemented inside the derived classes and are insulated via private implementations.
 	/// Each of the API's CRenderer classes which are derived from the CRendererBase class, are responsible for creating, accessing and freeing this resource type.
-	class CResourceVertexBufferBase
+	class CResourceFragmentProgramBase
 	{
 	public:
-		/// \brief Uploads the vertex buffer data stored into GPU memory, ready for use.
-		///
-		virtual void upload(void) = 0;
 
-		/// \brief Frees the data from the GPU memory.
-		virtual void free(void) = 0;
 	};
 
-	/// \brief OpenGL class for resource of type vertex buffer
+	/// \brief OpenGL class for resource of type fragment program
 	///
 	///
-	class CResourceVertexBufferOpenGL : public CResourceVertexBufferBase
+	class CResourceFragmentProgramOpenGL : public CResourceFragmentProgramOpenGL
 	{
 	public:
-		CResourceVertexBufferOpenGL();
-		~CResourceVertexBufferOpenGL();
+		CResourceFragmentProgramOpenGL();
+		~CResourceFragmentProgramOpenGL();
 
-		void upload(void);
-		void free(void);
 	private:
 		class CPrim;		///< Declaration of this class's private implementation holding things which we do not wish to expose when including this header file.
 							///< We put this here so we can create the pointer below, without having to include the header file containing the actual code.
 		CPrim* prim;		///< Pointer holding private implementation, new'd and delete'd in con/des tructors of this class.
 	};
 
-	/// \brief Vulkan class for resource of type vertex buffer
+	/// \brief Vulkan class for resource of type fragment program
 	///
 	///
-	class CResourceVertexBufferVulkan : public CResourceVertexBufferBase
+	class CResourceFragmentProgramVulkan : public CResourceFragmentProgramVulkan
 	{
 	public:
-		CResourceVertexBufferVulkan();
-		~CResourceVertexBufferVulkan();
+		CResourceFragmentProgramVulkan();
+		~CResourceFragmentProgramVulkan();
 
-		void upload(void);
-		void free(void);
 	private:
 		class CPrim;		///< Declaration of this class's private implementation holding things which we do not wish to expose when including this header file.
 		///< We put this here so we can create the pointer below, without having to include the header file containing the actual code.
--- ProjectFiles/StaticLibs/DavesCodeLib/Renderers/Resources/ResourceVertexProgram.h
+++ ProjectFiles/StaticLibs/DavesCodeLib/Renderers/Resources/ResourceVertexProgram.h
@@ -1,9 +1,9 @@
-#ifndef RESOURCE_VERTEXBUFFER_H
-#define RESOURCE_VERTEXBUFFER_H
+#ifndef RESOURCE_VERTEXPROGRAM_H
+#define RESOURCE_VERTEXPROGRAM_H
 
 namespace DCL
 {
-	/// \brief Base class for resource of type vertex buffer
+	/// \brief Base class for resource of type vertex program
 	///
 	/// We derive resource classes from this base class for each renderer type such as OpenGL and Vulkan.
 	/// These classes are to be declared within this header file to reduce the number of files.
@@ -12,45 +12,36 @@ namespace DCL
 	/// I know it means that the various renderers for the different graphics APIs are including stuff which isn't related to them, but that's a small price to pay for the reduced number of files we'd otherwise have to create.
 	/// Within DCL, all code uses only a pointer to the base class and the implementation for each of the methods are implemented inside the derived classes and are insulated via private implementations.
 	/// Each of the API's CRenderer classes which are derived from the CRendererBase class, are responsible for creating, accessing and freeing this resource type.
-	class CResourceVertexBufferBase
+	class CResourceVertexProgramBase
 	{
 	public:
-		/// \brief Uploads the vertex buffer data stored into GPU memory, ready for use.
-		///
-		virtual void upload(void) = 0;
 
-		/// \brief Frees the data from the GPU memory.
-		virtual void free(void) = 0;
 	};
 
-	/// \brief OpenGL class for resource of type vertex buffer
+	/// \brief OpenGL class for resource of type vertex program
 	///
 	///
-	class CResourceVertexBufferOpenGL : public CResourceVertexBufferBase
+	class CResourceVertexProgramOpenGL : public CResourceVertexProgramOpenGL
 	{
 	public:
-		CResourceVertexBufferOpenGL();
-		~CResourceVertexBufferOpenGL();
+		CResourceVertexProgramOpenGL();
+		~CResourceVertexProgramOpenGL();
 
-		void upload(void);
-		void free(void);
 	private:
 		class CPrim;		///< Declaration of this class's private implementation holding things which we do not wish to expose when including this header file.
 							///< We put this here so we can create the pointer below, without having to include the header file containing the actual code.
 		CPrim* prim;		///< Pointer holding private implementation, new'd and delete'd in con/des tructors of this class.
 	};
 
-	/// \brief Vulkan class for resource of type vertex buffer
+	/// \brief Vulkan class for resource of type vertex program
 	///
 	///
-	class CResourceVertexBufferVulkan : public CResourceVertexBufferBase
+	class CResourceVertexProgramVulkan : public CResourceVertexProgramVulkan
 	{
 	public:
-		CResourceVertexBufferVulkan();
-		~CResourceVertexBufferVulkan();
+		CResourceVertexProgramVulkan();
+		~CResourceVertexProgramVulkan();
 
-		void upload(void);
-		void free(void);
 	private:
 		class CPrim;		///< Declaration of this class's private implementation holding things which we do not wish to expose when including this header file.
 		///< We put this here so we can create the pointer below, without having to include the header file containing the actual code.
